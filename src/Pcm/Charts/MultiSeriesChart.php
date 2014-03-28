5ebd1314e69621d9b65aa779ba153eb6cd93f9ec
gmsantosxl <gabriel.ms1@hotmail.com>
2014-03-28 09:47:20 -0300

    Make PHP5.3 compatible - Sadly :(

--- src/Pcm/Charts/MultiSeriesChart.php
+++ src/Pcm/Charts/MultiSeriesChart.php
@@ -1,13 +1,28 @@
 <?php namespace Pcm\Charts;
 
 use Pcm\Charts\Libraries\FusionChartsLibrary;
+
 /**
  * Description of BasicChart
  *
  * @author gamacsan
  */
 class MultiSeriesChart extends AbstractChart{
-	use Traits\MultiSeriesTrait;
+//	use Traits\MultiSeriesTrait;
+	
+	/**
+	 * Belongs to Traits\MultiSeriesTrait;
+	 * 
+	 * @var type 
+	 */
+	protected $datasets = array();
+	
+	/**
+	 * Belongs to Traits\MultiSeriesTrait;
+	 * 
+	 * @var type 
+	 */
+	protected $datasetParams = array();
 	
 	public function __construct()
 	{
@@ -23,4 +38,43 @@ class MultiSeriesChart extends AbstractChart{
 		$this->createDataSeries($this->dataMatrix, $this->datasets, $this->datasetParams);
 	}
 
+	/**
+	 * Belongs to Traits\MultiSeriesTrait;
+	 * 
+	 * @param type $dataSet
+	 */
+	public function setDataSet($dataSet)
+	{
+		$this->datasets = $dataSet;
+	}
+
+	/**
+	 * Belongs to Traits\MultiSeriesTrait;
+	 * 
+	 * @param type $dataSet
+	 */
+	public function setDataSetParams($dataSetParams)
+	{
+		$this->datasetParams = $dataSetParams;
+	}
+	
+	/**
+	 * Belongs to Traits\MultiSeriesTrait;
+	 * 
+	 * @param type $dataSet
+	 */	
+	public function createDataSeries($dataMatrix, $datasets, $datasetParams)
+	{
+		foreach ($datasets as $key => $datasetName){
+			
+			// Make sure this dataset has params to inject
+			$dataSetParams = isset($datasetParams[$key]) ? $datasetParams[$key] : array();
+			
+			$this->chartLib->addDataset($datasetName, $dataSetParams);
+			foreach($dataMatrix[$key] as $value){
+				$this->chartLib->addChartData($value);
+			}
+		}
+	}
+	
 }
