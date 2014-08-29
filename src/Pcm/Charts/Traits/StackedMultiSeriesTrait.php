040eff92ee21e7faae8a13604a27d17b605200a7
gmsantosxl <gabriel.ms1@hotmail.com>
2014-08-29 16:17:49 -0300

    Extract refactor choose dataset type

--- src/Pcm/Charts/Traits/StackedMultiSeriesTrait.php
+++ src/Pcm/Charts/Traits/StackedMultiSeriesTrait.php
@@ -23,37 +23,42 @@ trait StackedMultiSeriesTrait {
 
 	public function builtMsStackedDataseries($dataArray, $datasets, $datasetParams)
 	{
-		foreach ($datasets as $key => $datasetName)
+		foreach ($datasets as $key => $datasetName) 
 		{
+			// No data to insert, move on
+			if (!isset($dataArray[$key])) {
+				continue;
+			}
+			
 			// Make sure this dataset has params to inject
 			$datasetConfig = isset($datasetParams[$key]) ? $datasetParams[$key] : [];
 
-			if (isset($datasetConfig['renderAs']) && $datasetConfig['renderAs'] == 'line')
-			{
-				$this->chartLib->addDatasetMSLine($datasetName, $datasetConfig);
-				$addDataMethod = 'addMSLinesetData';
-			}
-			else
-			{
-				$this->initializeMainDataset();
-				$this->chartLib->addSubDatasetMS($datasetName, $datasetConfig);
-				$addDataMethod = 'addChartData';
-			}
+			$addDataMethod = $this->chooseDataset($datasetName, $datasetConfig);
 
-			if (isset($dataArray[$key]))
+			foreach ($dataArray[$key] as $value) 
 			{
-				foreach ($dataArray[$key] as $value)
-				{
-					$this->chartLib->{$addDataMethod}($value);
-				}
+				$this->chartLib->{$addDataMethod}($value);
 			}
 		}
 	}
 
+	private function chooseDataset($datasetName, $datasetConfig)
+	{
+		if (isset($datasetConfig['renderAs']) && $datasetConfig['renderAs'] == 'line') {
+			$this->chartLib->addDatasetMSLine($datasetName, $datasetConfig);
+			$addDataMethod = 'addMSLinesetData';
+		} else {
+			$this->initializeMainDataset();
+			$this->chartLib->addSubDatasetMS($datasetName, $datasetConfig);
+			$addDataMethod = 'addChartData';
+		}
+
+		return $addDataMethod;
+	}
+
 	private function initializeMainDataset()
 	{
-		if (!$this->isMainDatasetInitialized)
-		{
+		if (!$this->isMainDatasetInitialized) {
 			$this->chartLib->createMSStDataset();
 			$this->isMainDatasetInitialized = true;
 		}
